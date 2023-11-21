bebb2403995a28421b1293e28b2513d55d534878
Dylan Knuth <dylan.knuth@oceus.io>
2023-11-20 23:49:36 -0800

    adding method GetFloat to config object

--- pkg/services/config/config_object.go
+++ pkg/services/config/config_object.go
@@ -103,6 +103,19 @@ func (n Object) GetInt(key string) int {
 	return int(val)
 }
 
+func (n Object) GetFloat64(key string) float64 {
+	if value, ok := n[key].(float64); ok {
+		return value
+	}
+
+	val, err := strconv.ParseFloat(fmt.Sprintf("%v", n[key]), 64)
+	if err != nil {
+		return 0
+	}
+
+	return val
+}
+
 func (n Object) GetBool(key string) bool {
 	value, _ := n[key].(bool)
 	return value
